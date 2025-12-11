f9831545f955dd3f73c3c909685cb7b352544506
Sysy's <sysys.iwi@gmail.com>
2025-12-11 16:02:32 +0100

    Add Player entity class
    Introduces the Player class to represent a user with a wallet and lists of current and selling games. Includes methods for managing wallet balance and game collections.

---
+++ src/main/java/fr/meow/simulator/core/entity/Player.java
@@ -0,0 +1,53 @@
+package fr.meow.simulator.core.entity;
+
+import fr.meow.simulator.core.SimulatorObject;
+import fr.meow.simulator.core.games.Game;
+
+import java.util.ArrayList;
+
+public class Player extends SimulatorObject {
+    private double wallet;
+    private ArrayList<Game> currentGames;
+    private ArrayList<Game> sellingGames;
+
+    public Player(double wallet) {
+        super("Player");
+
+        this.wallet = wallet;
+        this.currentGames = new ArrayList<>();
+        this.sellingGames = new ArrayList<>();
+    }
+
+    public double getWallet() {
+        return this.wallet;
+    }
+
+    public void addToWallet(double amount) {
+        this.wallet += wallet;
+    }
+
+    public void removeToWallet(double amount) {
+        this.wallet -= wallet;
+    }
+
+    public ArrayList<Game> getCurrentGames() {
+        return this.currentGames;
+    }
+
+    public ArrayList<Game> getSellingGames() {
+        return this.sellingGames;
+    }
+
+    public void addToCurrentGames(Game game) {
+        this.currentGames.add(game);
+    }
+
+    public void addToSellingGames(Game game) {
+        this.currentGames.remove(game);
+        this.sellingGames.add(game);
+    }
+
+    public void removeFromSelling(Game game) {
+        this.sellingGames.remove(game);
+    }
+}
