71d96961006dc418e096591066b74216a47051ef
Sysy's <sysys.iwi@gmail.com>
2025-12-12 15:14:23 +0100

    Refactor Colors enum to use uppercase names
    Updated the Colors enum to use uppercase constant names for consistency with Java naming conventions. Removed unused color values: magenta, pink, orange, and purple.

--- src/main/java/fr/meow/simulator/core/entity/client/Colors.java
+++ src/main/java/fr/meow/simulator/core/entity/client/Colors.java
@@ -1,13 +1,9 @@
 package fr.meow.simulator.core.entity.client;
 
 public enum Colors {
-    green,
-    red,
-    blue,
-    yellow,
-    cyan,
-    magenta,
-    pink,
-    orange,
-    purple
+    GREEN,
+    RED,
+    BLUE,
+    YELLOW,
+    CYAN,
 }
